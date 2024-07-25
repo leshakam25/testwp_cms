c0433d448204bfc4cc7bb4a80676fe7275b513e3
Alexei <leshakam25@gmail.com>
2024-07-25 14:00:10 +0300

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,46 +1,46 @@
-import { Providers } from '@/app/providers';
-import TheHeaderComponent from '@/theHeader/components/theHeader.component';
-import { Container, Grid, GridItem } from '@chakra-ui/react';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
-import 'normalize.css';
+import { Providers } from '@/app/providers'
+import TheHeaderComponent from '@/theHeader/components/theHeader.component'
+import { Container, Grid, GridItem } from '@chakra-ui/react'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import 'normalize.css'
 import React from 'react'
 
 // Инициализация шрифта
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] })
 
 // Метаданные приложения
 export const metadata: Metadata = {
 	applicationName: 'WPTest | Blog | applicationName',
 	title: 'WPTest | Blog',
-	description: 'WPTest | Blog | description',
-};
+	description: 'WPTest | Blog | description'
+}
 
 type RootLayoutProps = {
 	children: React.ReactNode;
 };
 
-const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
+const RootLayout = ({ children }: RootLayoutProps) => {
 	return (
-		<html lang="en">
+		<html lang='en'>
 		<body className={inter.className}>
 		<Providers>
 			<Grid
 				className={inter.className}
 				templateAreas={`"header" "main"`}
-				gridTemplateRows="auto 1fr"
-				gridTemplateColumns="1fr"
-				minH="100vh"
+				gridTemplateRows='auto 1fr'
+				gridTemplateColumns='1fr'
+				minH='100vh'
 			>
 				{/* Компонент заголовка */}
-				<GridItem area="header" as="header">
+				<GridItem area='header' as='header'>
 					<TheHeaderComponent />
 				</GridItem>
 				
 				{/* Основной контент */}
-				<GridItem area="main" as="main">
-					<Container maxW="full">
+				<GridItem area='main' as='main'>
+					<Container maxW='full'>
 						{children}
 					</Container>
 				</GridItem>
@@ -48,7 +48,7 @@ const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
 		</Providers>
 		</body>
 		</html>
-	);
-};
+	)
+}
 
-export default RootLayout;
+export default RootLayout
